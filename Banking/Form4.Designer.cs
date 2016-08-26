8573176a1e31fbff514b9a228f9115ad09fe8afa
sidverma32 <sid.verma32@gmail.com>
2016-08-26 23:30:57 +0530

    includes all functionality of bank(for employee)

---
+++ Banking/Form4.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace DBOperations
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(125, 142);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(136, 29);
+            this.textBox4.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 24);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Salary:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(76, 3);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(156, 24);
+            this.lblTitle.TabIndex = 22;
+            this.lblTitle.Text = "Employee Details";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(125, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 29);
+            this.textBox3.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 24);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Job:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 29);
+            this.textBox2.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Ename:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 29);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Eno:";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(154, 177);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(78, 35);
+            this.btnNext.TabIndex = 25;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(0, 177);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(78, 35);
+            this.btnFirst.TabIndex = 26;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(77, 177);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(78, 35);
+            this.btnPrev.TabIndex = 27;
+            this.btnPrev.Text = "Prev";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(231, 177);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(78, 35);
+            this.btnLast.TabIndex = 28;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(231, 212);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(78, 35);
+            this.btnDelete.TabIndex = 32;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(77, 212);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(78, 35);
+            this.btnInsert.TabIndex = 31;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(0, 212);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(78, 35);
+            this.btnNew.TabIndex = 30;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(154, 212);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(78, 35);
+            this.btnUpdate.TabIndex = 29;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(0, 247);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(155, 35);
+            this.btnSave.TabIndex = 33;
+            this.btnSave.Text = "Save to DB";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(154, 247);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(78, 35);
+            this.btnSearch.TabIndex = 34;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(231, 247);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(78, 35);
+            this.btnClose.TabIndex = 35;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 282);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ Banking/Form4.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic; 
+namespace DBOperations
+{
+    public partial class Form4 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommandBuilder cb;
+        DataSet ds;
+
+        int rno = 0;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("User Id=Sa;Password=123;Database=employee");
+            da = new SqlDataAdapter("Select Eno, Ename, Job, Salary From Employee Order By Eno", con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey; 
+            ds = new DataSet();
+            da.Fill(ds, "Employee");
+           // ds.WriteXml("C:\\CSharp11\\Employee.xml");
+            //ds.WriteXmlSchema("C:\\CSharp11\\Employee.xsd"); 
+            ShowData(); 
+        }
+        private void ShowData()
+        {
+            textBox1.Text = ds.Tables[0].Rows[rno][0].ToString();
+            textBox2.Text = ds.Tables[0].Rows[rno][1].ToString();
+            textBox3.Text = ds.Tables[0].Rows[rno][2].ToString();
+            textBox4.Text = ds.Tables[0].Rows[rno][3].ToString();
+        }
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            rno = 0;
+            ShowData(); 
+        }
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (rno > 0)
+            {
+                rno -= 1;
+                if (ds.Tables[0].Rows[rno].RowState == DataRowState.Deleted)
+               
+                
+                
+                {
+                    MessageBox.Show("Deleted records can't be accessed");
+                    return;
+                }
+                ShowData();
+            }
+            else
+                MessageBox.Show("First record of the table"); 
+        }
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (rno < ds.Tables[0].Rows.Count - 1)
+            {
+                rno += 1;
+                if (ds.Tables[0].Rows[rno].RowState == DataRowState.Deleted)
+                {
+                    MessageBox.Show("Deleted records can't be accessed");
+                    return;
+                }
+                ShowData();
+            }
+            else
+                MessageBox.Show("Last record of the table"); 
+        }
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            rno = ds.Tables[0].Rows.Count - 1;
+            ShowData(); 
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text = "";
+            int index = ds.Tables[0].Rows.Count - 1;
+            int eno = Convert.ToInt32(ds.Tables[0].Rows[index][0]) + 1;
+            textBox1.Text = eno.ToString();
+            textBox2.Focus(); 
+        }
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds.Tables[0].NewRow();
+            dr[0] = textBox1.Text; dr[1] = textBox2.Text;
+            dr[2] = textBox3.Text; dr[3] = textBox4.Text;
+            ds.Tables[0].Rows.Add(dr);
+            rno = ds.Tables[0].Rows.Count - 1;
+            MessageBox.Show("Record added to the DataTable of DataSet."); 
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ds.Tables[0].Rows[rno][1] = textBox2.Text;
+            ds.Tables[0].Rows[rno][2] = textBox3.Text;
+            ds.Tables[0].Rows[rno][3] = textBox4.Text;
+            MessageBox.Show("DataRow of DataTable updated."); 
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ds.Tables[0].Rows[rno].Delete();
+            MessageBox.Show("DataRow of DataTable deleted.");
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //da.InsertCommand.CommandText = "Insert Stmt";
+            //da.UpdateCommand.CommandText = "Update Stmt";
+            //da.DeleteCommand.CommandText = "Delete Stmt";
+
+            cb = new SqlCommandBuilder(da); 
+            da.Update(ds, "Employee");
+            MessageBox.Show("Data saved to database"); 
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string value = Interaction.InputBox("Enter Employee No to search");
+            int eno = int.Parse(value);
+            DataRow dr = ds.Tables[0].Rows.Find(eno);
+            if (dr != null)
+            {
+                textBox1.Text = dr[0].ToString();
+                textBox2.Text = dr[1].ToString();
+                textBox3.Text = dr[2].ToString();
+                textBox4.Text = dr[3].ToString();
+            }
+            else
+                MessageBox.Show("Employee doesn't exists."); 
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
